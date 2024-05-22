c490241134ee89d75a0be92e546fe8d2d8e25df7
devanandhatm <150226332+devanandhatm@users.noreply.github.com>
2024-05-22 14:19:50 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,63 @@
+// if(5<10){
+//     console.log("5 is less");
+// }
+// else{
+//     console.log("10 is greater");
+// }
+// let age = 20;
+// if (age>18) {
+//     console.log("you are eligible for driving licence");
+// } else {
+//     console.log("you are not eligible");
+    
+// }
+// let time = 20;
+// if(time<=12){
+//     console.log("Good morning");
+// }
+// else if(time>12 && time<=15){
+//     console.log("Good afternoon");
+// }
+// else if(time>15 && time<=19){
+//     console.log("Good evening");
+// }
+// else {
+//     console.log("Good night");
+// }
+// 
+// console.log(1);
+// console.log(2);
+// console.log(3);
+// console.log(4);
+// console.log(5);
+// 
+// for(let i=1;i<=5;i++){
+//     console.log(i)
+// }
+// for(let i=0;i<=10;i++){
+//     console.log(i)
+// }
+// let fruits =["Apple","Orange","Grapes","Banana"];
+// for(let i=0;i<fruits.length;i++){
+//     console.log(fruits[i]);
+// let num =[1,2,3,4,5,6];
+// for(let i=0;i<num.length;i++)
+//     if(num[i]%2==0){
+//         console.log(`${num[i]}is even`);
+// }
+
+// else{
+//     console.log(`${num[i]}is odd`);
+// }
+// find the sum of the array
+// let values = [1,2,3,4,5,6,7,8,9];
+// let sum=0;
+// for(let i=0;i<values.length;i++){
+//     sum = sum +values[i];
+// }
+// console.log(sum);
+// 1 to 5 
+// for(let i=1;i<=5;i++){
+//     for(j=1;j<=5;j++){
+//         console.log(`${i}*${j}=${i*j}`);
+//     }
